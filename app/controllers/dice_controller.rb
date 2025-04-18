5bf1309c12eb2e7cf6bc4304e8bb7c360d76c299
martaf793 <martaf@uchicago.edu>
2025-04-18 01:54:54 +0000

    flexible

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,5 +1,12 @@
 class DiceController < ApplicationController
 	def values
-	    render({:template => "game_templates/values"}) 
+    @n1_value=params.fetch("n1").to_i
+    @n2_value=params.fetch("n2").to_i
+    @rolls=[]
+    @n1_value.times do
+      n=rand(1..@n2_value)
+      @rolls.push(n)
+    end
+	  render({:template => "game_templates/values"}) 
   end
 end
